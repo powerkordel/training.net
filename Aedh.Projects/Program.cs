ff74840e7adc0eb032b90114bd34bcb8cd362bdc
Pawel <pawelkordel@gmail.com>
2022-02-10 05:56:12 +0100

    next portion of exe.

--- Aedh.Projects/Program.cs
+++ Aedh.Projects/Program.cs
@@ -2,7 +2,7 @@
 using System;
 using Aedh.Projects;
 {
-
+    
 
     Person pawel = new Person();
     pawel.FirstName = "Pawel";
@@ -92,35 +92,171 @@ c:\folder3\folder4";
 
 
 
-/*
- Your job is to write a program for a speed camera. 
-For simplicity, ignore the details such as camera, sensors, etc and focus purely on the logic. 
-Write a program that asks the user to enter the speed limit. 
-Once set, the program asks for the speed of a car. 
-If the user enters a value less than the speed limit, program should display Ok on the console. 
-If the value is above the speed limit, the program should calculate the number of demerit points. 
-For every 5km/hr above the speed limit, 1 demerit points should be incurred 
-and displayed on the console. 
-If the number of demerit points is above 12, the program should display License Suspended.
- */
+    /*
+     Your job is to write a program for a speed camera. 
+    For simplicity, ignore the details such as camera, sensors, etc and focus purely on the logic. 
+    Write a program that asks the user to enter the speed limit. 
+    Once set, the program asks for the speed of a car. 
+    If the user enters a value less than the speed limit, program should display Ok on the console. 
+    If the value is above the speed limit, the program should calculate the number of demerit points. 
+    For every 5km/hr above the speed limit, 1 demerit points should be incurred 
+    and displayed on the console. 
+    If the number of demerit points is above 12, the program should display License Suspended.
+     */
+    
+    double speedLimit, speedVehicle, punkty;
 
-double speedLimit, speedVehicle, punkty;
+        Console.WriteLine("What is a speed limit?");
 
-    Console.WriteLine("What is a speed limit?");
+        speedLimit = Convert.ToDouble(Console.ReadLine());
 
-    speedLimit = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("What is a speed of the car?");
 
-    Console.WriteLine("What is a speed of the car?");
+        speedVehicle = Convert.ToDouble(Console.ReadLine());
 
-    speedVehicle = Convert.ToDouble(Console.ReadLine());
+        if (speedVehicle <= speedLimit)
+        {
+            Console.WriteLine("OK");
+        }
+        else
+        {
+           punkty = (speedVehicle - speedLimit)/5;
+            Console.WriteLine("License suspended. You`ve got: " + punkty + " points");
+        }
 
-    if (speedVehicle <= speedLimit)
+        while (true)
+        {
+            Console.WriteLine("type your name: ");
+            var input3 = Console.ReadLine();
+
+            if (String.IsNullOrWhiteSpace(input3))
+            {
+                Console.WriteLine("@Echo: " + input3);
+                continue;
+            }
+                break;
+        }
+
+        
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////    
+    const int passwordLenght = 5;
+    var random = new Random();
+    var buffer = new char[passwordLenght];
+    for (var i = 0; i < passwordLenght; i++)
+        buffer[i] = (char)('a' + random.Next(0,26));
+    var password = new string(buffer);
+    Console.WriteLine(password);
+    
+
+    /*
+    1 - Write a program to count how many numbers between 1 and 100 are divisible by 3 with no remainder.
+    Display the count on the console.
+    */
+    
+    var licz = 0;
+    var przezTrzy = 0;
+
+    while (licz <= 100)
     {
-        Console.WriteLine("OK");
+        
+        if (licz % 3 == 0)
+        {
+            przezTrzy++;
+        }
+        licz++;
     }
-    else
+    Console.WriteLine(przezTrzy);
+    
+
+    /*
+     2- Write a program and continuously ask the user to enter a number or "ok" to exit. 
+    Calculate the sum of all the previously entered numbers and display it on the console.
+     */
+
+    
+        var total = 0;
+
+        while (true)
+        {
+            Console.WriteLine("Give a number: \nTo finish write \"ok\".");
+            var input1 = Console.ReadLine();
+
+            if (input1.ToLower() == "ok")
+                break;
+
+            total += Convert.ToInt32(input1);
+        }
+        Console.WriteLine("End. Total is: " + total);
+
+        
+
+    /*
+     3- Write a program and ask the user to enter a number. 
+     Compute the factorial of the number and print it on the console. 
+     For example, if the user enters 5, 
+     the program should calculate 5 x 4 x 3 x 2 x 1 and display it as 5! = 120.
+     */
+    
+    Console.WriteLine("enter number");
+    int n = 0;
+    int faktor= 1;
+    n = Convert.ToInt32(Console.ReadLine());
+    for (int i = n; i > 0; i--)
     {
-       punkty = (speedVehicle - speedLimit)/5;
-        Console.WriteLine("License suspended. You`ve got: " + punkty + " points");
+        faktor *= i;
     }
+    Console.WriteLine(n+"! = " + faktor);
+    
+
+
+    /*
+     4- Write a program that picks a random number between 1 and 10. 
+    Give the user 4 chances to guess the number. If the user guesses the number, 
+    display “You won"; otherwise, display “You lost". (To make sure the program is behaving correctly, 
+    you can display the secret number on the console first.)
+     */
+
+    
+    Console.WriteLine("Give a number: ");
+    var random1 = new Random();
+    
+    for (int i = 4; i > 0; i--)
+    {
+        int shot = Convert.ToInt32(Console.ReadLine());
+        int randi = random1.Next(1, 10);
+        Console.WriteLine("random number is : " + randi);
+        if (shot == randi)
+        {
+            Console.WriteLine("You won");
+            break;
+        }
+        else if (i == 1) Console.WriteLine("You lost");
+    }
+   
+    /*
+     5- Write a program and ask the user to enter a series of numbers separated by comma. 
+    Find the maximum of the numbers and display it on the console. 
+    For example, if the user enters “5, 3, 8, 1, 4", the program should display 8.
+     */
+
+    Console.WriteLine("Insert number separated by coma:");
+
+    var input = Console.ReadLine();
+
+    var numerx = input.Split(',');
+
+    var max = Convert.ToInt32(numerx[0]);
+
+    foreach (var str in numerx)
+    {
+        var number = Convert.ToInt32(str);
+        if (number > max)
+            max = number;
+    }
+    Console.WriteLine("max number is: " + max);
+
+
+
+
+
 }
